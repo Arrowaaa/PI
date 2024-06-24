818f56a7d68a600f4746fe63fea9d2a0eec74047
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2024-06-24 12:06:11 -0300

    melhorias no pi

--- text-PI/assets/js/mascaras.js
+++ text-PI/assets/js/mascaras.js
@@ -62,5 +62,23 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const togglePassword = document.querySelector('#mostrarSenha');
+    const password = document.querySelector('#senha');
 
+    togglePassword.addEventListener('click', function() {
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+        this.classList.toggle('oculto');
+    });
+
+    const toggleConfirmPassword = document.querySelector('#mostrarConfirmSenha');
+    const confirmPassword = document.querySelector('#confirmSenha');
+
+    toggleConfirmPassword.addEventListener('click', function() {
+        const type = confirmPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+        confirmPassword.setAttribute('type', type);
+        this.classList.toggle('oculto');
+    });
+});
 
