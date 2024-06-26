983ec8843708809742a686048e99e490088ed97e
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2024-06-26 11:28:48 -0300

    alteracoes no php

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
     <main class="container ">
         <div class="row ">
             <div class="col md-4 text-center">
-                <h5 class="font-weight-bold">MENU</h5>
+                <h5 class="font-weight-bold">Menu</h5>
                 <ul class="list-unstyled mt-3">
                     <li><a href="./index.php">Inicio</a></li>
                     <li><a href="./servicos.php">Serviços</a></li>
@@ -23,20 +23,33 @@
                 </ul>
             </div>
 
-            <div class="col-md-4 text-center">
-                <h5 class="font-weight-bold">Nossas Redes Sociais</h5>
-                <div class="social-icons mt-3">
-                    <a href="#"><i class="bi bi-youtube"></i></a>
-                    <a href="#"><i class="bi bi-whatsapp"></i></a>
-                    <a href="#"><i class="bi bi-instagram"></i></a>
-                    <a href="#"><i class="bi bi-facebook"></i></a>
-                </div>
+            <div class="col-md-2">
+                <h5>Parceiros</h5>
+                <ul class="list-unstyled mt-3">
+                    <li><a href="#" target="_blank">IDrink</a></li>
+                    <li><a href="https://ventqui.paulowh.com/" target="_blank">Ven't qui</a></li>
+                    <li><a href="https://doceslunares.paulowh.com/" target="_blank">Doces lunares</a></li>
+                    <li><a href="#" target="_blank">Manutenção</a></li>
+                    <li><a href="https://policebox.paulowh.com/" target="_blank">Police box</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5>Siga nos</h5>
+                <ul class="list-unstyled mt-3" id="icones">
+                    <a href="https://www.instagram.com"><i id="estilo" class="bi bi-instagram text-light"></i></a>
+                    <a href="https://www.youtube.com"><i id="estilo" class="bi bi-youtube text-light"></i></a>
+                    <a href="https://www.twitter.com"><i id="estilo" class="bi bi-twitter text-light"></i></a>
+                    <a href="https://github.com/Guilhlm"  target="_blank"><i id="estilo" class="bi bi-github"></i></a>
+                </ul>
             </div>
         </div>
+
         <div class="footer-bottom">
-            <p>&copy;2024. Este site foi desenvolvido como projeto integrador (PI) - SENAC Americana.<br>
-                Não reservamos os
-                direitos e valores apresentados são meramentes ilustrativos para fins educacionais.</p>
+            <div>
+                &copy; 2024. Este site foi desenvolvido por Guilherme Maia da turma TI21 - SENAC AMERICANA. <br> Não
+                reservamos os
+                direitos e valores apresentados, são meramente ilustrativos para fins educacionais.
+            </div>
         </div>
     </main>
 </footer>
--- raiox.php
+++ raiox.php
@@ -1,36 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Raio-X - Ven't qui</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="assets/css/principal.css">
-</head>
-
-<body>
-    <header>
-        <nav class="menu-perfil">
-            <img src="assets/img/logo principal.png" alt="logo da empresa" class="logo">
-            <p>“Unindo amor e expertise para cuidar dos seus melhores amigos na saúde e na alegria."</p>
-            <ul class="menu">
-                <li><a href="index.php">Início</a></li>
-                <li class="dropdown">
-                    <a class="linha" href="#" class="dropbtn">Requisições &#9660;</a>
-                    <div class="dropdown-content">
-                        <a class="linha" href="raiox.php">Raio-X</a>
-                        <a href="internacao.php">Internação</a>
-                    </div>
-                </li>
-                <li><a href="servicos.php">Serviços</a></li>
-                <li><a href="sobre.php">Sobre</a></li>
-                <li class="icon"><a href="login.php"><i class="bi bi-person"> Login</i></a></li>
-            </ul>
-        </nav>
-    </header>
+<?php include './includes/header.php' ?>
 
     <main style="display:flex; align-items: center;">
         <section class="container mt-5">
@@ -143,41 +111,6 @@
             </div>
         </section>
     </main>
-    <br>
-    <footer class="rodape">
-        <main class="container">
-            <div class="row">
-                <div class="col md-4 text-center">
-                    <h5 class="font-weight-bold">MENU</h5>
-                    <ul class="list-unstyled mt-3">
-                        <li><a href="./index.php">Inicio</a></li>
-                        <li><a href="./servicos.php">Serviços</a></li>
-                        <li><a href="./sobre.php">Sobre</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4 text-center">
-                    <h5 class="font-weight-bold">Sobre a Gente</h5>
-                    <ul class="list-unstyled mt-3">
-                        <li><a href="#">Rua: Germano Giusti, 26 - Jd. Paulista - Americana - SP</a></li>
-                        <li><a href="#">Contato</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4 text-center">
-                    <h5 class="font-weight-bold">Nossas Redes Sociais</h5>
-                    <div class="social-icons mt-3">
-                        <a href="#"><i class="bi bi-youtube"></i></a>
-                        <a href="#"><i class="bi bi-whatsapp"></i></a>
-                        <a href="#"><i class="bi bi-instagram"></i></a>
-                        <a href="#"><i class="bi bi-facebook"></i></a>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-bottom">
-                <p>&copy;2024. Este site foi desenvolvido como projeto integrador (PI) - SENAC Americana.<br>
-                    Não reservamos os direitos e valores apresentados são meramente ilustrativos para fins educacionais.</p>
-            </div>
-        </main>
-    </footer>
-</body>
+    
+    <?php include './includes/footer.php' ?>
 
-</html>
