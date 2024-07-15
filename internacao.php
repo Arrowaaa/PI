1aed35ce07623b9783187741a28db07c1830e9a2
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2024-07-14 22:32:22 -0300

    melhorias

--- internacao.php
+++ internacao.php
@@ -1,138 +1,130 @@
 <?php include './includes/header.php'; ?>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.2/html2pdf.bundle.min.js" integrity="sha512-MpDFIChbcXl2QgipQrt1VcPHMldRILetapBl5MPCA9Y8r7qvlwx1/Mc9hNTzY+kS5kX6PdoDq41ws1HiVNLdZA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<main style="display:flex; align-items: center;">
-    <section class="container mt-5">
+<main >
+    <section id="box">
         <div id="content">
-            <form id="pet-internment-form">
-                <h1>Preencha Seus Dados Da Internação</h1>
+            <form id="form">
+                <h2>Preencha Seus Dados Da Internação</h2>
                 <br>
 
                 <div class="mb-3">
                     <label for="nome" class="form-label">Nome:</label>
-                    <input type="text" class="form-control" id="nome" placeholder="Digite seu Nome" name="nome">
+                    <input type="text" class="inputUser" id="nome" name="nome">
                 </div>
 
                 <div class="mb-3">
                     <label for="cpf" class="form-label">CPF:</label>
-                    <input type="text" class="form-control" id="cpf" placeholder="Digite seu CPF" name="cpf">
+                    <input type="text" class="inputUser" id="cpf"  name="cpf">
                 </div>
 
                 <div class="mb-3">
                     <label for="telefone" class="form-label">Telefone:</label>
-                    <input type="number" class="form-control" id="telefone" placeholder="Digite seu número" name="numerotelefonico">
+                    <input type="number" class="inputUser" id="telefone"  name="numerotelefonico">
                 </div>
 
                 <div class="mb-3">
                     <label for="email" class="form-label">E-mail:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Digite Seu Email" name="email">
+                    <input type="email" class="inputUser" id="email" name="email">
                 </div>
 
                 <div class="mb-3">
                     <label for="data_nascimento" class="form-label">Data de Nascimento:</label>
-                    <input type="date" class="form-control" id="data_nascimento" name="data_nascimento">
+                    <input type="date" class="inputUser" id="data_nascimento" name="data_nascimento">
                 </div>
 
-                <div class="input-group">
-                    <label for="sexo">Sexo do Tutor: </label>
-                    <select id="sexo" name="sexo">
-                        <option></option>
-                        <option value="m">Masculino</option>
-                        <option value="f">Feminino</option>
-                        <option value="o">Outros</option>
-                    </select>
+                <div class="mb-3">
+                    <label for="sexo">Sexo do Tutor:  </label>
+                    <input type="radio">  Masculino  </input>
+                    <input type="radio">  Feminino  </input>
+                    <input type="radio">  Outros  </input>
                 </div>
 
                 <div class="mb-3">
                     <label for="endereco" class="form-label">Endereço:</label>
-                    <input type="text" class="form-control" id="endereco" placeholder="Digite seu Endereço" name="endereco">
+                    <input type="text" class="inputUser" id="endereco"  name="endereco">
                 </div>
 
                 <div class="mb-3">
                     <label for="pet_nome" class="form-label">Nome do Pet:</label>
-                    <input type="text" class="form-control" id="pet_nome" placeholder="Digite o Nome do Pet" name="pet_nome">
+                    <input type="text" class="inputUser" id="pet_nome"  name="pet_nome">
                 </div>
 
                 <div class="mb-3">
                     <label for="pet_especie" class="form-label">Espécie do Pet:</label>
-                    <input type="text" class="form-control" id="pet_especie" placeholder="Digite a Espécie do Pet" name="pet_especie">
+                    <input type="text" class="inputUser" id="pet_especie" name="pet_especie">
                 </div>
 
                 <div class="mb-3">
                     <label for="pet_raca" class="form-label">Raça do Pet:</label>
-                    <input type="text" class="form-control" id="pet_raca" placeholder="Digite a Raça do Pet" name="pet_raca">
+                    <input type="text" class="inputUser" id="pet_raca" name="pet_raca">
                 </div>
 
                 <div class="mb-3">
                     <label for="pet_idade" class="form-label">Idade do Pet:</label>
-                    <input type="number" class="form-control" id="pet_idade" placeholder="Digite a idade do Pet" name="pet_idade">
+                    <input type="number" class="inputUser" id="pet_idade" name="pet_idade">
                 </div>
 
                 <div class="mb-3">
-                    <div class="input-group">
-                        <label for="pet_sexo">Gênero do Pet: </label>
-                        <select id="pet_sexo" name="pet_sexo">
-                            <option></option>
-                            <option value="m">Macho</option>
-                            <option value="f">Fêmea</option>
-                            <option value="o">Outros</option>
-                        </select>
-                    </div>
+                    <label for="sexopet">Sexo do Pet:  </label>
+                    <input type="radio">  Macho  </input>
+                    <input type="radio">  Femea  </input>
+                    <input type="radio">  Outros  </input>
                 </div>
 
                 <div class="mb-3">
                     <label for="motivo_internacao" class="form-label">Motivo da Internação:</label>
-                    <textarea class="form-control" id="motivo_internacao" name="motivo_internacao" cols="30" rows="3"></textarea>
+                    <textarea class="inputUser" id="motivo_internacao" name="motivo_internacao" cols="30" rows="3"></textarea>
                 </div>
 
                 <div class="mb-3">
                     <label for="historico_medico" class="form-label">Histórico Médico:</label>
-                    <textarea class="form-control" id="historico_medico" name="historico_medico" cols="30" rows="3"></textarea>
+                    <textarea class="inputUser" id="historico_medico" name="historico_medico" cols="30" rows="3"></textarea>
                 </div>
 
                 <div class="mb-3">
                     <label for="medicacoes" class="form-label">Medicações Atuais:</label>
-                    <textarea class="form-control" id="medicacoes" name="medicacoes" cols="30" rows="3"></textarea>
+                    <textarea class="inputUser" id="medicacoes" name="medicacoes" cols="30" rows="3"></textarea>
                 </div>
 
                 <div class="mb-3">
                     <label for="alergias" class="form-label">Alergias:</label>
-                    <textarea class="form-control" id="alergias" name="alergias" cols="30" rows="3"></textarea>
+                    <textarea class="inputUser" id="alergias" name="alergias" cols="30" rows="3"></textarea>
                 </div>
 
                 <div class="mb-3">
                     <label for="contato_emergencia" class="form-label">Contato de Emergência:</label>
-                    <input type="text" class="form-control" id="contato_emergencia" placeholder="Nome do Contato" name="contato_emergencia">
-                    <input type="number" class="form-control mt-2" id="contato_telefone" placeholder="Telefone do Contato" name="contato_telefone">
+                    <input type="text" class="inputUser" id="contato_emergencia"  name="contato_emergencia">
+                    
                 </div>
 
                 <div class="mb-3">
                     <label for="preferencia_data_hora" class="form-label">Preferência de Data e Hora:</label>
-                    <input type="datetime-local" class="form-control" id="preferencia_data_hora" name="preferencia_data_hora">
+                    <input type="datetime-local" class="inputUser" id="preferencia_data_hora" name="preferencia_data_hora">
                 </div>
 
                 <div class="mb-3">
                     <label for="mensagem" class="form-label">Mensagem:</label>
-                    <textarea class="form-control" id="mensagem" name="mensagem" cols="30" rows="5" placeholder="Deixe uma mensagem"></textarea>
+                    <textarea class="inputUser" id="mensagem" name="mensagem" cols="30" rows="5"></textarea>
                 </div>
 
                 <div class="mb-3">
                     <label for="estado_saude" class="form-label">Estado de Saúde Atual:</label>
-                    <textarea class="form-control" id="estado_saude" name="estado_saude" cols="30" rows="3"></textarea>
+                    <textarea class="inputUser" id="estado_saude" name="estado_saude" cols="30" rows="3"></textarea>
                 </div>
 
                 <div class="mb-3">
                     <label for="alimentacao" class="form-label">Alimentação:</label>
-                    <textarea class="form-control" id="alimentacao" name="alimentacao" cols="30" rows="3"></textarea>
+                    <textarea class="inputUser" id="alimentacao" name="alimentacao" cols="30" rows="3"></textarea>
                 </div>
 
                 <div class="mb-3">
                     <label for="medicacao_previa" class="form-label">Medicação Prévia:</label>
-                    <textarea class="form-control" id="medicacao_previa" name="medicacao_previa" cols="30" rows="3"></textarea>
+                    <textarea class="inputUser" id="medicacao_previa" name="medicacao_previa" cols="30" rows="3"></textarea>
                 </div>
 
                 <div class="mb-3">
                     <label for="necessidades_especiais" class="form-label">Necessidades Especiais ou Cuidados Específicos:</label>
-                    <textarea class="form-control" id="necessidades_especiais" name="necessidades_especiais" cols="30" rows="3"></textarea>
+                    <textarea class="inputUser" id="necessidades_especiais" name="necessidades_especiais" cols="30" rows="3"></textarea>
                 </div>
 
                 <div class="mb-3 form-check">
